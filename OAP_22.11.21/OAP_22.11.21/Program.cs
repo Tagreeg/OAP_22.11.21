d0a8ecfd768232fcf3b0d08733d7aeab3a149327
Tagreeg <94824491+Tagreeg@users.noreply.github.com>
2021-11-22 11:55:28 +0500

    Add files via upload

---
+++ OAP_22.11.21/OAP_22.11.21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAP_22._11._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite razmer massiva");
+            int size = 0, n;
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("OShibka vvoda. Vvedite chislo");
+            }
+            Console.WriteLine("Vvedite chisla v massiv");
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                while (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Oshibka vvoda. Vvedite chislo");
+                }
+                array[i] = n;
+            }
+            Console.WriteLine();
+
+            var selected = from p in array
+                           where (p % 2) == 0
+                           select p;
+            foreach (int s in selected)
+            {
+                Console.Write(s + " ");
+            }
+        }
+    }
+}
---
+++ OAP_22.11.21/Zadanie 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Zadanie_2
+{
+    /*class Number
+    {
+        public float Num { get; set; }
+        public int Pos { get; set; }
+        public float Sqr { get; set; }
+    }*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite razmer massiva");
+            int size = 0;
+            float n;
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("OShibka vvoda. Vvedite chislo");
+            }
+            Console.WriteLine("Vvedite chisla v massiv");
+            float[] array = new float[size];
+            for (int i = 0; i < size; i++)
+            {
+                while (!float.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Oshibka vvoda. Vvedite chislo");
+                }
+                array[i] = n;
+            }
+            Console.WriteLine();
+
+
+            var selectedNumber = from a in array
+                           where (a * a) > 25
+                           select a;
+
+            foreach (int d in selectedNumber)
+            {
+                int ind = Array.IndexOf(array, d);
+                Console.WriteLine("chislo " + d + " ind " + ind + " kv " + d * d);
+            }
+        }
+    }
+}
